thvnx/mpfr

commit f00ee49865ef438db862c557c7b4d2ed5a8e9fc9

Added new types mpfr_limb_ptr and mpfr_limb_srcptr to src/mpfr-impl.h, and replaced mp_ptr and mp_srcptr (GMP internals) by these 2 new types respectively.
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@7455 280ebfd0-de03-0410-8827-d642c229c3f4

src/mpfr-gmp.h
@@ -289,8 +289,8 @@ __MPFR_DECLSPEC void __gmp_default_free _MPFR_PROTO ((void *, size_t));
 
 #if defined(WANT_GMP_INTERNALS) && defined(HAVE___GMPN_ROOTREM)
 #ifndef __gmpn_rootrem
-  __MPFR_DECLSPEC mp_size_t __gmpn_rootrem _MPFR_PROTO ((mp_ptr, mp_ptr,
-                                             mp_srcptr, mp_size_t, mp_limb_t));
+  __MPFR_DECLSPEC mp_size_t __gmpn_rootrem _MPFR_PROTO ((mpfr_limb_ptr,
+                    mpfr_limb_ptr, mpfr_limb_srcptr, mp_size_t, mp_limb_t));
 #endif
 #endif
 

src/div.c
@@ -26,7 +26,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 #ifdef DEBUG2
 #define mpfr_mpn_print(ap,n) mpfr_mpn_print3 (ap,n,MPFR_LIMB_ZERO)
 static void
-mpfr_mpn_print3 (mp_ptr ap, mp_size_t n, mp_limb_t cy)
+mpfr_mpn_print3 (mpfr_limb_ptr ap, mp_size_t n, mp_limb_t cy)
 {
   mp_size_t i;
   for (i = 0; i < n; i++)
@@ -40,7 +40,7 @@ mpfr_mpn_print3 (mp_ptr ap, mp_size_t n, mp_limb_t cy)
 
 /* check if {ap, an} is zero */
 static int
-mpfr_mpn_cmpzero (mp_ptr ap, mp_size_t an)
+mpfr_mpn_cmpzero (mpfr_limb_ptr ap, mp_size_t an)
 {
   while (an > 0)
     if (MPFR_LIKELY(ap[--an] != MPFR_LIMB_ZERO))
@@ -53,7 +53,8 @@ mpfr_mpn_cmpzero (mp_ptr ap, mp_size_t an)
    Takes into account bp[0] for extra=1.
 */
 static int
-mpfr_mpn_cmp_aux (mp_ptr ap, mp_size_t an, mp_ptr bp, mp_size_t bn, int extra)
+mpfr_mpn_cmp_aux (mpfr_limb_ptr ap, mp_size_t an,
+                  mpfr_limb_ptr bp, mp_size_t bn, int extra)
 {
   int cmp = 0;
   mp_size_t k;
@@ -109,7 +110,8 @@ mpfr_mpn_cmp_aux (mp_ptr ap, mp_size_t an, mp_ptr bp, mp_size_t bn, int extra)
    Return borrow out.
 */
 static mp_limb_t
-mpfr_mpn_sub_aux (mp_ptr ap, mp_ptr bp, mp_size_t n, mp_limb_t cy, int extra)
+mpfr_mpn_sub_aux (mpfr_limb_ptr ap, mpfr_limb_ptr bp, mp_size_t n,
+                  mp_limb_t cy, int extra)
 {
   mp_limb_t bb, rp;
 
@@ -137,11 +139,11 @@ mpfr_div (mpfr_ptr q, mpfr_srcptr u, mpfr_srcptr v, mpfr_rnd_t rnd_mode)
   mp_size_t qsize; /* number of limbs of the computed quotient */
   mp_size_t qqsize;
   mp_size_t k;
-  mp_ptr q0p = MPFR_MANT(q), qp;
-  mp_ptr up = MPFR_MANT(u);
-  mp_ptr vp = MPFR_MANT(v);
-  mp_ptr ap;
-  mp_ptr bp;
+  mpfr_limb_ptr q0p = MPFR_MANT(q), qp;
+  mpfr_limb_ptr up = MPFR_MANT(u);
+  mpfr_limb_ptr vp = MPFR_MANT(v);
+  mpfr_limb_ptr ap;
+  mpfr_limb_ptr bp;
   mp_limb_t qh;
   mp_limb_t sticky_u = MPFR_LIMB_ZERO;
   mp_limb_t low_u;
@@ -419,7 +421,7 @@ mpfr_div (mpfr_ptr q, mpfr_srcptr u, mpfr_srcptr v, mpfr_rnd_t rnd_mode)
                  r + low(u) has qsize + (usize-2*qsize) = usize-qsize limbs */
             {
               mp_size_t l;
-              mp_ptr sp;
+              mpfr_limb_ptr sp;
               int cmp_s_r;
               mp_limb_t qh2;
 

src/eq.c
@@ -28,7 +28,7 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 int
 mpfr_eq (mpfr_srcptr u, mpfr_srcptr v, unsigned long int n_bits)
 {
-  mp_srcptr up, vp;
+  mpfr_limb_srcptr up, vp;
   mp_size_t usize, vsize, size, i;
   mpfr_exp_t uexp, vexp;
   int k;

src/init2.c
@@ -26,7 +26,7 @@ void
 mpfr_init2 (mpfr_ptr x, mpfr_prec_t p)
 {
   mp_size_t xsize;
-  mp_ptr tmp;
+  mpfr_limb_ptr tmp;
 
   /* Check if we can represent the number of limbs
    * associated to the maximum of mpfr_prec_t*/
@@ -52,7 +52,7 @@ mpfr_init2 (mpfr_ptr x, mpfr_prec_t p)
   MPFR_ASSERTN(p >= MPFR_PREC_MIN && p <= MPFR_PREC_MAX);
 
   xsize = (mp_size_t) ((p - 1) / GMP_NUMB_BITS) + 1;
-  tmp   = (mp_ptr) (*__gmp_allocate_func)(MPFR_MALLOC_SIZE(xsize));
+  tmp   = (mpfr_limb_ptr) (*__gmp_allocate_func)(MPFR_MALLOC_SIZE(xsize));
 
   MPFR_PREC(x) = p;                /* Set prec */
   MPFR_EXP (x) = MPFR_EXP_INVALID; /* make sure that the exp field has a

src/mpfr-impl.h
@@ -158,6 +158,15 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
 
 
 /******************************************************
+ ************* GMP Basic Pointer Types ****************
+ ******************************************************/
+
+typedef mp_limb_t *mpfr_limb_ptr;
+typedef __gmp_const mp_limb_t *mpfr_limb_srcptr;
+
+
+
+/******************************************************
  ****************** (U)INTMAX_MAX *********************
  ******************************************************/
 
@@ -1737,13 +1746,14 @@ __MPFR_DECLSPEC void mpfr_clear_cache _MPFR_PROTO ((mpfr_cache_t));
 __MPFR_DECLSPEC int  mpfr_cache _MPFR_PROTO ((mpfr_ptr, mpfr_cache_t,
                                               mpfr_rnd_t));
 
-__MPFR_DECLSPEC void mpfr_mulhigh_n _MPFR_PROTO ((mp_ptr, mp_srcptr,
-                                                  mp_srcptr, mp_size_t));
-__MPFR_DECLSPEC void mpfr_mullow_n  _MPFR_PROTO ((mp_ptr, mp_srcptr,
-                                                  mp_srcptr, mp_size_t));
-__MPFR_DECLSPEC void mpfr_sqrhigh_n _MPFR_PROTO ((mp_ptr, mp_srcptr, mp_size_t));
-__MPFR_DECLSPEC mp_limb_t mpfr_divhigh_n _MPFR_PROTO ((mp_ptr qp, mp_ptr np,
-                                                     mp_ptr dp, mp_size_t n));
+__MPFR_DECLSPEC void mpfr_mulhigh_n _MPFR_PROTO ((mpfr_limb_ptr,
+                        mpfr_limb_srcptr, mpfr_limb_srcptr, mp_size_t));
+__MPFR_DECLSPEC void mpfr_mullow_n  _MPFR_PROTO ((mpfr_limb_ptr,
+                        mpfr_limb_srcptr, mpfr_limb_srcptr, mp_size_t));
+__MPFR_DECLSPEC void mpfr_sqrhigh_n _MPFR_PROTO ((mpfr_limb_ptr,
+                        mpfr_limb_srcptr, mp_size_t));
+__MPFR_DECLSPEC mp_limb_t mpfr_divhigh_n _MPFR_PROTO ((mpfr_limb_ptr,
+                        mpfr_limb_ptr, mpfr_limb_ptr, mp_size_t));
 
 __MPFR_DECLSPEC int mpfr_round_p _MPFR_PROTO ((mp_limb_t *, mp_size_t,
                                                mpfr_exp_t, mpfr_prec_t));
@@ -1758,7 +1768,7 @@ __MPFR_DECLSPEC int mpfr_round_near_x _MPFR_PROTO ((mpfr_ptr, mpfr_srcptr,
 __MPFR_DECLSPEC void mpfr_abort_prec_max _MPFR_PROTO ((void))
        MPFR_NORETURN_ATTR;
 
-__MPFR_DECLSPEC void mpfr_rand_raw _MPFR_PROTO((mp_ptr, gmp_randstate_t,
+__MPFR_DECLSPEC void mpfr_rand_raw _MPFR_PROTO((mpfr_limb_ptr, gmp_randstate_t,
                                                 unsigned long));
 
 __MPFR_DECLSPEC mpz_t* mpfr_bernoulli_internal _MPFR_PROTO((mpz_t*,

src/mulders.c
@@ -39,7 +39,8 @@ static short mulhigh_ktab[] = {MPFR_MULHIGH_TAB};
 /* Put in  rp[n..2n-1] an approximation of the n high limbs
    of {up, n} * {vp, n}. The error is less than n-1 ulps of rp[n]. */
 static void
-mpfr_mulhigh_n_basecase (mp_ptr rp, mp_srcptr up, mp_srcptr vp, mp_size_t n)
+mpfr_mulhigh_n_basecase (mpfr_limb_ptr rp, mpfr_limb_srcptr up,
+                         mpfr_limb_srcptr vp, mp_size_t n)
 {
   mp_size_t i;
 
@@ -55,7 +56,8 @@ mpfr_mulhigh_n_basecase (mp_ptr rp, mp_srcptr up, mp_srcptr vp, mp_size_t n)
 /* Put in  rp[n..2n-1] an approximation of the n high limbs
    of {np, n} * {mp, n}. The error is less than n-1 ulps of rp[n]. */
 void
-mpfr_mulhigh_n (mp_ptr rp, mp_srcptr np, mp_srcptr mp, mp_size_t n)
+mpfr_mulhigh_n (mpfr_limb_ptr rp, mpfr_limb_srcptr np, mpfr_limb_srcptr mp,
+                mp_size_t n)
 {
   mp_size_t k;
 
@@ -98,7 +100,7 @@ static short sqrhigh_ktab[] = {MPFR_SQRHIGH_TAB};
 /* Put in  rp[n..2n-1] an approximation of the n high limbs
    of {np, n}^2. The error is less than n ulps of rp[n]. */
 void
-mpfr_sqrhigh_n (mp_ptr rp, mp_srcptr np, mp_size_t n)
+mpfr_sqrhigh_n (mpfr_limb_ptr rp, mpfr_limb_srcptr np, mp_size_t n)
 {
   mp_size_t k;
 
@@ -140,11 +142,12 @@ static short divhigh_ktab[] = {MPFR_DIVHIGH_TAB};
    THIS IS PRELIMINARY CODE, DO NOT USE IT.
 */
 mp_limb_t
-mpfr_divhigh_n (mp_ptr qp, mp_ptr np, mp_ptr dp, mp_size_t n)
+mpfr_divhigh_n (mpfr_limb_ptr qp, mpfr_limb_ptr np, mpfr_limb_ptr dp,
+                mp_size_t n)
 {
   mp_size_t k, l;
   mp_limb_t qh, cy;
-  mp_ptr tp;
+  mpfr_limb_ptr tp;
   MPFR_TMP_DECL(marker);
 
   k = divhigh_ktab[n];

src/rec_sqrt.c
@@ -70,8 +70,8 @@ http://www.gnu.org/licenses/ or write to the Free Software Foundation, Inc.,
    http://www.loria.fr/~zimmerma/mca/pub226.html
 */
 static void
-mpfr_mpn_rec_sqrt (mp_ptr x, mpfr_prec_t p,
-                   mp_srcptr a, mpfr_prec_t ap, int as)
+mpfr_mpn_rec_sqrt (mpfr_limb_ptr x, mpfr_prec_t p,
+                   mpfr_limb_srcptr a, mpfr_prec_t ap, int as)
 
 {
   /* the following T1 and T2 are bipartite tables giving initial
@@ -192,7 +192,7 @@ mpfr_mpn_rec_sqrt (mp_ptr x, mpfr_prec_t p,
   else /* p >= 12 */
     {
       mpfr_prec_t h, pl;
-      mp_ptr r, s, t, u;
+      mpfr_limb_ptr r, s, t, u;
       mp_size_t xn, rn, th, ln, tn, sn, ahn, un;
       mp_limb_t neg, cy, cu;
       MPFR_TMP_DECL(marker);
@@ -421,7 +421,7 @@ mpfr_rec_sqrt (mpfr_ptr r, mpfr_srcptr u, mpfr_rnd_t rnd_mode)
   mpfr_prec_t rp, up, wp;
   mp_size_t rn, wn;
   int s, cy, inex;
-  mp_ptr x;
+  mpfr_limb_ptr x;
   MPFR_TMP_DECL(marker);
 
   MPFR_LOG_FUNC (("x[%#R]=%R rnd=%d", u, u, rnd_mode),

src/round_prec.c
@@ -69,7 +69,7 @@ mpfr_prec_round (mpfr_ptr x, mpfr_prec_t prec, mpfr_rnd_t rnd_mode)
       if (nw > ow)
        {
          /* Realloc significand */
-         mp_ptr tmpx = (mp_ptr) (*__gmp_reallocate_func)
+         mpfr_limb_ptr tmpx = (mpfr_limb_ptr) (*__gmp_reallocate_func)
            (MPFR_GET_REAL_PTR(x), MPFR_MALLOC_SIZE(ow), MPFR_MALLOC_SIZE(nw));
          MPFR_SET_MANT_PTR(x, tmpx); /* mant ptr must be set
                                         before alloc size */

commit 02a72df35a763481063a3c32abb4fd5ea2a378b9

[src/mpfr-gmp.h] fixed bug with --enable-gmp-internals introduced in r7455 		 (mpfr_limb_ptr is not yet defined when reading mpfr-gmp.h 		  thus we can't use it)
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@7858 280ebfd0-de03-0410-8827-d642c229c3f4

doc/README.dev
@@ -174,9 +174,9 @@ To make a release (for the MPFR team):
      and without GMP's --disable-alloca configure option (or compile GMP
      with --enable-alloca=debug and MPFR with --with-gmp-build to be
      able to get the memory leak errors), with and without -DXDEBUG in
-     $CFLAGS, with and without gmp internal files, with and without
-     GMP built as a shared library, with and without srcdir equal to
-     objdir (../mpfr/configure --srcdir=/users/spaces/pelissip/mpfr/),
+     $CFLAGS, with and without gmp internal files (--enable-gmp-internals),
+     with and without GMP built as a shared library, with and without srcdir
+     equal to objdir (../mpfr/configure --srcdir=/users/spaces/pelissip/mpfr/),
      with and without --enable-logging.
 
      Try different gcc versions with different options: with and without

src/mpfr-gmp.h
@@ -289,8 +289,8 @@ __MPFR_DECLSPEC void __gmp_default_free _MPFR_PROTO ((void *, size_t));
 
 #if defined(WANT_GMP_INTERNALS) && defined(HAVE___GMPN_ROOTREM)
 #ifndef __gmpn_rootrem
-  __MPFR_DECLSPEC mp_size_t __gmpn_rootrem _MPFR_PROTO ((mpfr_limb_ptr,
-                    mpfr_limb_ptr, mpfr_limb_srcptr, mp_size_t, mp_limb_t));
+  __MPFR_DECLSPEC mp_size_t __gmpn_rootrem _MPFR_PROTO ((mp_limb_t*,
+                    mp_limb_t*, mp_limb_t*, mp_size_t, mp_limb_t));
 #endif
 #endif
 